669d705ca281c42a126f9209508c149671fb016c
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-22 13:59:45 +0600

    feat: complete user register functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,12 @@ import bcrypt from 'bcrypt';
 import validator from 'validator'
 
 
+// ---- token create ------
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+
 // ----- login user -----
 const loginUser = async (req, res) => {
   
@@ -14,7 +20,47 @@ const loginUser = async (req, res) => {
 
 // ------- register user ------ 
 const registerUser = async (req, res) => {
+  const { name, password, email } = req.body
   
+  try {
+    // --- checking user is alreasy exists or not ---
+    const exist = await userModel.findOne({ email });
+    if (exist) {
+      return res.json({success:false, message:"User already exists"})
+    }
+
+    // ------- checking valid email & strong password -----
+    if (!validator.isEmail(email)) {
+      return res.json({success:false, message:"Please enter a valid email"})
+    }
+    // --- password ---
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: 'password must be at least 8 characters',
+      });
+    }
+
+    // ---- hashing user password ----
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassWord = await bcrypt.hash(password, salt)
+
+    // ---- creating new user -------
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password:hashedPassWord
+    })
+    
+    const user = await newUser.save()
+    const token = createToken(user._id)
+
+    res.json({success:true, token})
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:"Error!"})
+  }
 }
 
 
