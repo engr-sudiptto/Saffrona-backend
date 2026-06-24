8ce4a19eee930bdf69997fda553d1ba43ec03655
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-24 22:53:38 +0600

    feat: complete order process with stripe

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import authMiddleware from './../middleware/auth.js';
-import { placeOrder } from "../controllers/orderController.js";
+import { placeOrder, verifyOrder } from "../controllers/orderController.js";
 
 
 const orderRoute = express.Router()
 
 
 orderRoute.post("/place", authMiddleware, placeOrder)
+orderRoute.post('/verify', verifyOrder);
 
 
 export default orderRoute
