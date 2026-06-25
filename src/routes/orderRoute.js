fd3917990933aacb275abd6d534abcb622aacbf7
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-25 12:05:02 +0600

    feat: implement user order functionality for frontenf

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from './../middleware/auth.js';
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 
 const orderRoute = express.Router()
@@ -8,6 +8,6 @@ const orderRoute = express.Router()
 
 orderRoute.post("/place", authMiddleware, placeOrder)
 orderRoute.post('/verify', verifyOrder);
-
+orderRoute.post('/userorders', authMiddleware, userOrders)
 
 export default orderRoute
